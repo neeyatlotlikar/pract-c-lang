daf687f71c7ac31020fd3538689c29da900a8427
neeyatlotlikar <neeyat.lotlikar@gmail.com>
2025-12-29 16:23:05 +0530

    create list implemented

--- linkedlists/single_linked_list.c
+++ linkedlists/single_linked_list.c
@@ -8,9 +8,11 @@ struct node {
 
 void display(struct node *start);
 struct node *create_list(struct node *start);
-void search(struct node *start, int data);
-void *delete(struct node *start, int data);
-void count(struct node *start);
+// void search(struct node *start, int data);
+// void *delete(struct node *start, int data);
+// void count(struct node *start);
+struct node *append(struct node *start, int data);
+struct node *topup(struct node *start, int data);
 
 int main(void) {
 	struct node *start = NULL;
@@ -21,8 +23,11 @@ int main(void) {
 		printf("2. Display\n");
 		printf("3. Count\n");
 		printf("4. Search\n");
-		printf("5. Delete\n");
-		printf("6. Quit\n");
+		printf("5. Add a node to an empty list or to the beginning of "
+		       "the list\n");
+		printf("6. Add an element to the list's end\n");
+		printf("7. Delete\n");
+		printf("8. Quit\n");
 		printf("Enter your chosen option: ");
 		scanf("%d", &choice);
 
@@ -34,19 +39,29 @@ int main(void) {
 			display(start);
 			break;
 		case 3:
-			count(start);
+			// count(start);
 			break;
 		case 4:
 			printf("Enter data to search for: ");
 			scanf("%d", &data);
-			search(start, data);
+			// search(start, data);
 			break;
 		case 5:
-			printf("Enter data to delete: ");
+			printf("Enter a value at the list's beginning: ");
 			scanf("%d", &data);
-			delete (data);
+			topup(start, data);
 			break;
 		case 6:
+			printf("Enter a value at the list's ending: ");
+			scanf("%d", &data);
+			append(start, data);
+			break;
+		case 7:
+			printf("Enter data to delete: ");
+			scanf("%d", &data);
+			// delete (start, data);
+			break;
+		case 8:
 			exit(EXIT_SUCCESS);
 		default:
 			printf("Please choose a valid option.\n");
@@ -69,4 +84,45 @@ void display(struct node *start) {
 		p = p->link;
 	}
 	printf("\n\n");
+}
+
+struct node *topup(struct node *start, int data) {
+	struct node *tmp;
+	tmp = (struct node *)malloc(sizeof(struct node));
+	tmp->info = data;
+	tmp->link = start;
+	start = tmp;
+	return start;
+}
+
+struct node *append(struct node *start, int data) {
+	struct node *p, *tmp;
+	tmp = (struct node *)malloc(sizeof(struct node));
+	tmp->info = data;
+	p = start;
+	while (p->link != NULL) {
+		p = p->link;
+	}
+	p->link = tmp;
+	tmp->link = NULL;
+	return start;
+}
+
+struct node *create_list(struct node *start) {
+	int i, n, data;
+	printf("How many nodes should the list possess? ");
+	scanf("%d", &n);
+	start = NULL;
+	if (n == 0) {
+		return start;
+	}
+	printf("Enter the value you want to insert: ");
+	scanf("%d", &data);
+	start = topup(start, data);
+	for (i = 2; i <= n; i++) {
+		printf("Enter the value you want to insert: ");
+		scanf("%d", &data);
+		start = append(start, data);
+	}
+	return start;
 }
